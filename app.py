8d439fa1c3e397121d205a864713ac5dcdc1e8e6
Growthack90 <hackmaker90@outlook.it>
2024-07-31 11:37:11 +0200

    add functionality Authorization & Authentication

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from userModel import User, users
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Sostituisci con una chiave segreta più sicura
 
-# Users data for example purposes
-users = {
-    'admin': 'password123'
-}
+# Configura Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(username):
+    return User.get(username)
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        return redirect(url_for('home'))
     return render_template('index.html')
 
 @app.route('/home')
+@login_required
 def home():
-    if 'username' in session:
-        return render_template('home.html', username=session['username'])
-    return redirect(url_for('login'))
+    return render_template('home.html', username=current_user.id)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username in users and users[username] == password:
-            session['username'] = username
+        user = User.get(username)
+        if user and users[username]['password'] == password:
+            login_user(user)
             flash('Login successful!', 'success')
             return redirect(url_for('home'))
         else:
@@ -34,8 +38,9 @@ def login():
     return render_template('login.html')
 
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('username', None)
+    logout_user()
     flash('You have been logged out.', 'info')
     return redirect(url_for('index'))
 
--- readme.md
+++ readme.md
@@ -10,6 +10,7 @@
 - Install dependencies:
     * pip install setuptools
     * pip install flask
+    * pip install flask-login
 
 - Check if installed correctly:
     * pip list
